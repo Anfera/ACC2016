b88d1623864287353dac71d660324a9b4c8c8d39
Andres <af.ramirez236@gmail.com>
2015-09-01 18:04:59 -0500

    Cambios al codigo de MPC

--- MPC.m
+++ MPC.m
@@ -18,7 +18,7 @@ T = 0.1;               % Esta variable es el horizonte de prediccion en horas
 tspan = t;             % Tiempo de simulacion definido en SWMM
 x0 = zeros(6,1);       % Los tanques empiezan descargados
 vmax = vmax/3600;      % El 3600 es por la integración utilizada
-N = ceil(T/dt);        % Horizonte de predicción
+N = round(T/dt);        % Horizonte de predicción
 d = dt*[inflows(:,1) inflows(:,2) zeros(size(inflows(:,1))) inflows(:,3) inflows(:,4) zeros(size(inflows(:,1)))]'; % Perturmaciones
 
 a1 = -0;
@@ -47,35 +47,12 @@ nx = size(A,1); % Number of states
 nu = size(B,2); % Number of inputs
 
 % MPC data
-Q1 = 50*[1 0 -1 0 0 0; 0 0 0 0 0 0; -1 0 1 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0];
-Q2 = 50*[0 0 0 0 0 0; 0 1 -1 0 0 0; 0 -1 1 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0];
-Q3 = 50*[0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 1 0 0 -1; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 -1 0 0 1];
-Q4 = 50*[0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 1 0 -1; 0 0 0 0 0 0; 0 0 0 -1 0 1];
-Q5 = 50*[0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 1 -1; 0 0 0 0 -1 1];
+Q1 = [1 0 -1 0 0 0; 0 0 0 0 0 0; -1 0 1 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0];
+Q2 = [0 0 0 0 0 0; 0 1 -1 0 0 0; 0 -1 1 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0];
+Q3 = [0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 1 0 0 -1; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 -1 0 0 1];
+Q4 = [0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 1 0 -1; 0 0 0 0 0 0; 0 0 0 -1 0 1];
+Q5 = [0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 1 -1; 0 0 0 0 -1 1];
 
 Q = Q1 + Q2 + Q3 + Q4 + Q5;
+Q = 50*Q;
 R = eye(size(B,2));
-
-X = zeros(size(A,1),length(tspan));
-X(:,1) = x0;
-
-for i = 1:length(tspan)-1
-    [i length(tspan)-1]
-    
-    u = sdpvar(repmat(nu,1,N),repmat(1,1,N));
-    X0 = sdpvar(6,1);
-
-    constraints = [];
-    objective = 0;
-    x = X0;
-    for k = 1:N
-     x = A*x + B*u{k};
-     objective = objective + norm(Q*x,1) + norm(R*u{k},1);
-     %constraints = [constraints, -1 <= u{k}<= 1, -5<=x<=5];
-    end
-
-    optimize([constraints, X0 == X(:,i)],objective);
-    U = value(u{1})
-    
-    X(:,i+1) = A*X(:,i) + B*U + d(:,i);
-end
